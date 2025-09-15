21b581928fcf8952dc11ac52f2fa4dc4518162d3
Gsocram <202507287192@alunos.estacio.br>
2025-09-15 01:06:32 +0000

    switch

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
  
 
  int main(){
@@ -12,43 +15,8 @@
  float area = 150.75f;
  float pib = 639000000.0f;
  int numerodepontosturisticos = 100;
- float densidadePopulacional;
- float pibpercapita;
-
- printf("Digite o Estado:"); 
- scanf("%s", Estado);
-
-
- printf("Digite o Codigo:");
- scanf("%s", codigo);
-
-
- printf("Digite o Nome da Cidade:");
- scanf("%s", nomedacidade);
-
-
- printf("Digite o numero de População:");
- scanf("%d", &populacao);
-
-
- printf("Digite o Tamnho da Àrea:");
- scanf("%f", &area);
-
-
- printf("Digite o PIB:");
- scanf("%f", &pib);
-
-
- printf("Digite o Numero de Pontos Turisticos:");
- scanf("%d", &numerodepontosturisticos);
-
-
- densidadePopulacional = (float) populacao / area;
- printf("Densidade Populacional: %f hab/km²\n", densidadePopulacional);
-
- pibpercapita = (float) pib / populacao;
- printf("PIB Per Capita: %f Reais\n", pibpercapita);
-
+ float densidadePopulacional = populacao / area;
+ float pibpercapita = 28483.0f;
 
  printf("carta2:\n");
 
@@ -59,8 +27,8 @@
  float Area = 160.70f;
  float Pib = 729000000.0f;
  int numerodepontosTuristicos = 50;
- float densidadepopulacional;
- float Pibpercapita;
+ float densidadepopulacional = populacaO / Area;
+ float Pibpercapita = 2059.0f;
 
 
  printf("Digite o Estado:");
@@ -97,65 +65,43 @@
  printf("PIB Per  capita: %f Reais\n", pibpercapita);
 
  
- if(populacao > populacaO){
-  printf(" Carta 1  Venveu!\n");
- }else{
-    printf(" Carta 2  Venceu!\n");
- }
- printf("Comparação de Cartas (Atributo:População)\n");
- printf("Carta 1 - Baiha(BA):%d\n", populacao);
- printf("Carta 2 - Goiás(go):%d\n", populacaO);
- printf("Resultado: Carta 1 (Bahia)Venceu!\n");
+ 
+ //printf("Comparação de Cartas (Atributo:População)\n");
+ //printf("Carta 1 - Baiha(BA):%d\n", populacao);
+ //printf("Carta 2 - Goiás(go):%d\n", populacaO);
+ //printf("Resultado: Carta 1 (Bahia)Venceu!\n");
 
- if(area > Area){
-   printf("Carta 1 Venceu!\n");
- }else{
-   printf("Carta 2 Venceu!\n");
+ 
  }
- printf("Comparação de cartas (Atributo: Area)\n");
- printf("Carta 1 - Bahia(BA):%f\n",area);
- printf("Carta 2 - Goiás(GO):%f\n",Area);
- printf("Resultado: Carta 2 (Goiás) Venceu!\n");
+ //printf("Comparação de cartas (Atributo: Area)\n");
+// printf("Carta 1 - Bahia(BA):%f\n",area);
+// printf("Carta 2 - Goiás(GO):%f\n",Area);
+ //printf("Resultado: Carta 2 (Goiás) Venceu!\n");
 
- if(pib > Pib){
-    printf("Carta 1 Venceu!\n");
- }else{
-    printf("Carta 2 Venceu!\n");
- }
-  printf("Comparação de Cartas (Atributo: PIB)\n");
-  printf("Carta 1 - Bahia(BA):%f\n", pib);
-  printf("Catra 2 - Goiás(GO):%f\n",Pib);
-  printf("Resultado: Carta 2 (Goiás) Venceu!\n");
+ 
+  //printf("Comparação de Cartas (Atributo: PIB)\n");
+  //printf("Carta 1 - Bahia(BA):%f\n", pib);
+  //printf("Catra 2 - Goiás(GO):%f\n",Pib);
+  //printf("Resultado: Carta 2 (Goiás) Venceu!\n");
 
- if(numerodepontosturisticos > numerodepontosTuristicos){
-    printf("Carta 1 Venceu!\n");
- }else{
-    printf("Carta 2 Venceu!\n");
- }
-   printf("Comparação de Cartas (Atributo: Numero de pontos Turistico)\n");
-   printf("Carta 1 - Bahia(BA):%d\n",numerodepontosturisticos);
-   printf("Carta 2 - Goiás(GO):%d\n",numerodepontosTuristicos);
-   printf("Resultado: Carta 1 (Bahia) Venceu\n");
 
- if(densidadePopulacional < densidadepopulacional){
-    printf("Carta 1 Venceu!\n");
- }else{
-    printf("Carta 2 Venceu!\n");
- }
-   printf("Comparação de Cartas (Atributo: Densidade Populacional)\n ");
-   printf("Carta 1 - Bahia(BA):%f\n", densidadePopulacional);
-   printf("Carta 2 - Goiás(GO):%f\n", densidadepopulacional);
-   printf("Resultado: Carta 2 (Goiás) Venceu!\n");
+   //printf("Comparação de Cartas (Atributo: Numero de pontos Turistico)\n");
+   //printf("Carta 1 - Bahia(BA):%d\n",numerodepontosturisticos);
+   //printf("Carta 2 - Goiás(GO):%d\n",numerodepontosTuristicos);
+   //printf("Resultado: Carta 1 (Bahia) Venceu\n");
 
- if(pibpercapita > Pibpercapita){
-    printf("Carta 1 Venceu!\n");
- }else{
-    printf("Carta 2 Venceu!\n");
+ 
+   //printf("Comparação de Cartas (Atributo: Densidade Populacional)\n ");
+   //printf("Carta 1 - Bahia(BA):%f\n", densidadePopulacional);
+   //printf("Carta 2 - Goiás(GO):%f\n", densidadepopulacional);
+   //printf("Resultado: Carta 2 (Goiás) Venceu!\n");
+
+ 
  }
-    printf("Comparação de Cartas (Atributo:Pib per capita\n)");
-    printf("Carta 1 - Baiha(BA):%f\n", pibpercapita);
-    printf("Carta 2 - Goiás(GO):%f\n",Pibpercapita);
-    printf("Resultado: Carta 1 (Bahia)Venceu!");
+    //printf("Comparação de Cartas (Atributo:Pib per capita\n)");
+    //printf("Carta 1 - Baiha(BA):%f\n", pibpercapita);
+    //printf("Carta 2 - Goiás(GO):%f\n",Pibpercapita);
+   // printf("Resultado: Carta 1 (Bahia)Venceu!");
 
 
   
